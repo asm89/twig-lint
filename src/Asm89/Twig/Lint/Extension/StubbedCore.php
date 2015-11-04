77b2f224bf1e523f6a74cafcb27c9cff7ef02091
Johan Rouve <johan.rouve@canaltp.fr>
2015-11-04 11:01:49 +0100

    Allow 2 words tests

--- src/Asm89/Twig/Lint/Extension/StubbedCore.php
+++ src/Asm89/Twig/Lint/Extension/StubbedCore.php
@@ -11,6 +11,9 @@
 
 namespace Asm89\Twig\Lint\Extension;
 
+use \Twig_Parser;
+use \Twig_Token;
+
 /**
  * Overridden core extension to stub tests.
  *
@@ -39,5 +42,32 @@ class StubbedCore extends \Twig_Extension_Core
         } catch (\Twig_Error_Syntax $exception) {
             return 'null';
         }
+    }
+
+    protected function getTest(Twig_Parser $parser, $line)
+    {
+        $stream = $parser->getStream();
+        $name = $stream->expect(Twig_Token::NAME_TYPE)->getValue();
+        $env = $parser->getEnvironment();
+
+        if ($stream->test(Twig_Token::NAME_TYPE)) {
+            // try 2-words tests
+            $name = $name.' '.$parser->getCurrentToken()->getValue();
+
+            if ($test = $env->getTest($name)) {
+                $parser->getStream()->next();
+
+                return array($name, $test);
+            }
+        }
+
+        if ($test = $env->getTest($name)) {
+            return array($name, $test);
+        }
+
+        $e = new Twig_Error_Syntax(sprintf('Unknown "%s" test.', $name), $line, $parser->getFilename());
+        $e->addSuggestions($name, array_keys($env->getTests()));
+
+        throw $e;
     }
 }
