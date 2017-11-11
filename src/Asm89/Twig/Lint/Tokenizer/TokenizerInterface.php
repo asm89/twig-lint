1ba77edc85f328d854377549912a4dff44ff298a
Hussard <adrien.ricartnoblet@gmail.com>
2017-11-11 17:18:35 +0100

    Add copyright on all new files!

--- src/Asm89/Twig/Lint/Tokenizer/TokenizerInterface.php
+++ src/Asm89/Twig/Lint/Tokenizer/TokenizerInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of twig-lint.
+ *
+ * (c) Alexander <iam.asm89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Asm89\Twig\Lint\Tokenizer;
 
 interface TokenizerInterface
