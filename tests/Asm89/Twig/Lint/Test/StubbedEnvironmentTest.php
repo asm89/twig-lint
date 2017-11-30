76fe4d7d2e0f50e7f13c9408ee78dbc5e0528a7a
Hussard <adrien.ricartnoblet@gmail.com>
2017-11-30 17:13:27 +0100

    Forward-compatibility for phpunit and PHP7

--- tests/Asm89/Twig/Lint/Test/StubbedEnvironmentTest.php
+++ tests/Asm89/Twig/Lint/Test/StubbedEnvironmentTest.php
@@ -12,12 +12,13 @@
 namespace Asm89\Twig\Lint\Test;
 
 use Asm89\Twig\Lint\StubbedEnvironment;
+use PHPUnit\Framework\TestCase;
 use \Twig_Error;
 
 /**
  * @author Alexander <iam.asm89@gmail.com>
  */
-class StubbedEnvironmentTest extends \PHPUnit_Framework_TestCase
+class StubbedEnvironmentTest extends TestCase
 {
     private $env;
 
